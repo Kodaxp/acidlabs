2b00c561d97a5e6206808fad57cf4ce75559cd2b
Joel Zúñiga <ext_jandzunigam@falabella.cl>
2022-12-21 16:09:41 -0300

    feat(): project doctors work

--- src/doctor/controller/doctor.controller.ts
+++ src/doctor/controller/doctor.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import { DoctorService } from '../service/doctor.service';
 
 @Controller('doctor')
@@ -13,5 +13,25 @@ export class DoctorController {
   @Post('/create-new-doctor')
   async createDoctor(@Body() doctorReq: any) {
     return await this.doctorService.create(doctorReq);
+  }
+
+  @Get('/find-doctor/:id')
+  async findDoctor(@Param() params) {
+    return await this.doctorService.find(params.id);
+  }
+
+  @Get('/find-doctors')
+  async findAllDoctors() {
+    return await this.doctorService.findAll();
+  }
+
+  @Put('/update-doctor/:id')
+  async updateDoctor(@Param() params, @Body() doctorReq: any) {
+    return await this.doctorService.update(params.id, doctorReq);
+  }
+
+  @Delete('/delete-doctor/:id')
+  async deleteDoctor(@Param() params) {
+    return await this.doctorService.delete(params.id);
   }
 }
--- src/doctor/service/doctor.service.ts
+++ src/doctor/service/doctor.service.ts
@@ -18,4 +18,27 @@ export class DoctorService {
     await this.doctorRepository.insert(doctor);
     return { create: 'success', doctor };
   }
+
+  async find(id: any) {
+    return await this.doctorRepository.findOne({ where: { id } });
+  }
+
+  async findAll() {
+    return await this.doctorRepository.find();
+  }
+
+  async update(id: number, doctor: any) {
+    await this.doctorRepository.update(id, doctor);
+    return {
+      update: 'success',
+      doctor
+    }
+  }
+
+  async delete(id: number) {
+    await this.doctorRepository.delete(id);
+    return {
+      delete: 'success'
+    }
+  }
 }
